fb1843e70ae533df9db62851342010828c2e25ca
GCgang <powm97@gmail.com>
2024-06-30 20:08:48 +0900

    Feat: type과 styled components 사용해보기

---
+++ src/components/Circle.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+export default function Circle({ bgColor, borderColor }: CircleProps) {
+  const [counter, setCounter] = useState(0);
+  return (
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? bgColor}
+    ></Container>
+  );
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Circle from './components/Circle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Circle borderColor="yellow" bgColor="teal" />
+    <Circle bgColor="tomato" />
   </React.StrictMode>
 );
